94fa326d448d665373eb576c378ca76c42bf1dfc
Huy Nguyen <nqhuy2k6@gmail.com>
2015-08-08 12:10:59 +0700

    Anh Man Fix demo

--- app/page/pages/system-detail.js
+++ app/page/pages/system-detail.js
@@ -25,6 +25,7 @@ define(['durandal/app', "knockout", "bootstrap", "viewmodels/component-2",
             d[0].id = l?l[0].id:d[0].id;
             return d;
         }
+		
         me.activate = function (sys, id) {
             var config = [
                 {
@@ -37,9 +38,49 @@ define(['durandal/app', "knockout", "bootstrap", "viewmodels/component-2",
             me.compoment1(new Component2(config));
             id != undefined?me.compoment2(new Component5(initbuff(id,""))):me.compoment2(new ComponentDetailSystem(initbuff(sys,"_DETAIL")));
             var Alarms = [];
-            me.compoment3(new Component4(Alarms));
-                
+            me.compoment3(new Component4(Alarms));    
         };
+		
+		////////////////////////////////
+		function test(channel){
+			if(jQuery("#sys_left_close").hasClass( "sys_open" )){
+				
+				jQuery("#sys_detail_compoment1").hide();
+			
+				jQuery("#sys_detail_left").removeClass("col-md-3");
+				jQuery("#sys_detail_right").removeClass("col-md-9");
+				
+				jQuery("#sys_detail_left").toggleClass("col-md-1");
+				jQuery("#sys_detail_right").toggleClass("col-md-11");
+				
+				jQuery("#sys_left_close").removeClass("sys_open");
+				jQuery("#sys_left_close").toggleClass("sys_close");
+				
+				jQuery("#sys_detail_left").attr("style","width:4%");
+				jQuery(".sys_icon_close").attr("style","width:100%");
+				jQuery("#i-icon").toggleClass("fa-chevron-circle-right");
+				jQuery("#sys_detail_right").attr("style","width:96%");
+				
+			} else {
+				jQuery("#sys_detail_compoment1").show();
+			
+				jQuery("#sys_detail_left").removeClass("col-md-1");
+				jQuery("#sys_detail_right").removeClass("col-md-11");
+				
+				jQuery("#sys_detail_left").toggleClass("col-md-3");
+				jQuery("#sys_detail_right").toggleClass("col-md-9");
+				
+				jQuery("#sys_left_close").removeClass("sys_close");
+				jQuery("#sys_left_close").toggleClass("sys_open");
+				
+				jQuery("#sys_detail_left").attr("style","");
+				jQuery(".sys_icon_close").attr("style","width:15%;float: right;");
+				jQuery("#i-icon").toggleClass("fa-chevron-circle-right");
+				jQuery("#sys_detail_right").attr("style","");
+			}
+        }
+		me.test = test;		
+		////////////////////////////////////////  
 
     };
 }
